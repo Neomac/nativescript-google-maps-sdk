7ca579ec594f0d23ca132f36e6e4c74f88d2fed8
dev <chapreri@gmail.com>
2016-08-23 12:31:52 +0200

    animate from latLng

--- map-view.android.js
+++ map-view.android.js
@@ -84,7 +84,7 @@ var MapView = (function (_super) {
         var cameraUpdate = com.google.android.gms.maps.CameraUpdateFactory.newCameraPosition(cp);
         this.gMap.animateCamera(cameraUpdate);
     };
-    MapView.prototype.animateFromLatLng() = function (latitude, longitude) {
+    MapView.prototype.animateFromLatLng = function (latitude, longitude) {
         var cpBuilder = new com.google.android.gms.maps.model.CameraPosition.Builder();
         var cp = null;
         cpBuilder.target(new com.google.android.gms.maps.model.LatLng(latitude, longitude));
